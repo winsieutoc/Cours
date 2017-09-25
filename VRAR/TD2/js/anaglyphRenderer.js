74b6344fd4edca620afc17ac360e0fee12b9618e
axelwolski <axelwolski92@gmail.com>
2017-09-25 17:58:50 +0200

    add RVAR registory and TD2

---
+++ VRAR/TD2/js/anaglyphRenderer.js
@@ -0,0 +1,18 @@
+function AnaglyphRenderer ( renderer ) {
+
+  // left and right cameras
+  this.cameraLeft  = new THREE.Camera();
+  this.cameraLeft.matrixAutoUpdate = false;
+  this.cameraRight = new THREE.Camera();
+  this.cameraRight.matrixAutoUpdate = false;
+
+
+	this.update = function ( camera ) {
+    // TODO
+	}
+
+  this.render = function ( scene, camera ) {
+    // TODO
+  }
+
+}
