0b81e9c837c35f74818c956379aade80eb79384d
axelwolski <axel.wolski@etu.u-bordeaux.fr>
2017-09-29 12:47:16 +0200

    modif tp amip

--- AMIP/tp1/Assignement.m
+++ AMIP/tp1/Assignement.m
@@ -7,8 +7,10 @@ S = im2double(S);
 sizeT = [128, 128];
 T = zeros(sizeT(1), sizeT(2), 3);
 
-n = size(S)(1);
-m = size(S)(2);
+sizeS = size(S);
+
+n = sizeS(1);
+m = sizeS(2);
 
 L = 3;
 
@@ -17,8 +19,10 @@ patch = S(randi(n-L+1)+(0:L-1),randi(m-L+1)+(0:L-1),:);
 figure
 imshow(patch)
 
-centerW = size(T)(1) / 2;
-centerH = size(T)(2) / 2;
+sizet = size(T);
+
+centerW = sizet(1) / 2;
+centerH = sizet(2) / 2;
 
 T(centerW-1:centerW+1, centerH-1:centerH+1, :) = patch;
 
@@ -39,21 +43,21 @@ imshow(M)
 se = strel('square', L);
 min = 0;
 %while M(M == 0)
-for q=1:4
-
-  L = M - imdilate(M, se);
-  for x=1:size(L)(1)
-    for y=1:size(L)(2)
-      if L(x,y,:) == 1
-        for i=1:size(S)(1)
-          for j=1:size(S)(2)
-            SSD(S(i,j), T(x,y));
-            min = SSD;
-          end
-        end
-      
-
-end
+% for q=1:4
+% 
+%   L = M - imdilate(M, se);
+%   for x=1:size(L)(1)
+%     for y=1:size(L)(2)
+%       if L(x,y,:) == 1
+%         for i=1:size(S)(1)
+%           for j=1:size(S)(2)
+%             SSD(S(i,j), T(x,y));
+%             min = SSD;
+%           end
+%         end
+%       
+% 
+% end
 
 %M = zeros(sizeT(1), sizeT(2), 3);
 %
