3341c62d421efd1a5f1119ddce0c174c8b2696d9
axelwolski <axel.wolski@etu.u-bordeaux.fr>
2017-11-06 15:49:57 +0100

    aimp

---
+++ AMIP/tp3/GCMex/minCut.m
@@ -0,0 +1,18 @@
+%tp3
+
+%%%%%%%%%%%%%%%%%%%%%% MINCUT %%%%%%%%%%%%%%%%%%%%%%%%%
+
+close all
+clear
+
+A = imread('text1.jpg');
+A = im2double(A);
+sizeA = size(A);
+
+B = A;
+sizeB = sizeA;
+
+figure(2)
+imagesc(B);
+
+
