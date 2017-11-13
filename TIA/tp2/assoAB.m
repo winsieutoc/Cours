f8bba44433ea5c439819221658c40e878687e8ba
axelwolski <axelwolski92@gmail.com>
2017-11-13 19:03:52 +0100

    cleanning git Cours

--- TIA/tp2/assoAB.m
+++
@@ -1,8 +0,0 @@
-function [ a ] = assoAB( image, sizeP, x, y )
-%ASSOAB Summary of this function goes here
-%   Detailed explanation goes here
-
-    a = image(randi(x-sizeP+1)+(0:sizeP-1),randi(y-sizeP+1)+(0:sizeP-1),:);
-
-end
-
