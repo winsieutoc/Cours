8bb2ae8b9dd9f97d75f63d8f07a4622938037dbd
axelwolski <axel.wolski@etu.u-bordeaux.fr>
2017-11-14 13:49:35 +0100

    tia

--- TIA/tp3/GCMex/minCut.m
+++ TIA/tp3/GCMex/minCut.m
@@ -8,11 +8,24 @@ clear
 A = imread('text1.jpg');
 A = im2double(A);
 sizeA = size(A);
+widthSize = Size(A,1);
+heightSize = Size(A,2);
 
 B = A;
 sizeB = sizeA;
 
-figure(2)
-imagesc(B);
+overlap = 8;
+
+M = zeros((widthSize-overlap)+widthSize, heightSize);
+sizeM = size(M);
+
+firstPartM = a(:, heightSize-overlap+1:heightSize,:);
+secondPartM = a(:, 1:overlap, :);
+
+overlapErr = overlapError(firstPartM, secondPartM);
 
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%labelcost: delta c = ( 0 1 )
+%                     ( 1 0 )
