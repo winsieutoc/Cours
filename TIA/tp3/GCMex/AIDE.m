ee2528a84a0fcdf09a339f9e5f9e8c119da24cb7
axelwolski <axel.wolski@etu.u-bordeaux.fr>
2017-11-17 15:17:15 +0100

    tia astuce

---
+++ TIA/tp3/GCMex/AIDE.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%% AIDE %%%%%%%%%%%%%%%%%%%%%%
+
+% pairwise = sparse(N,N)
+
+% indsub
+% subind
+% i <=> (x,y)
+
+% pairwise(i,j) = j <=> (x+1,y)
+%                       (x-1,y)
+%                       (x,y+1)
+%                       (x,y-1)
+
+% ||A(i)-B(i)||(² en bas) + ||A(j)-B(j)||
