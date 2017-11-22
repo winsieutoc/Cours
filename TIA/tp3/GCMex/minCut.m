f81cb4556722b99a9a2768d6c01886494fb83246
axelwolski <axel.wolski@etu.u-bordeaux.fr>
2017-11-22 20:38:55 +0100

    tp3

--- TIA/tp3/GCMex/minCut.m
+++ TIA/tp3/GCMex/minCut.m
@@ -8,23 +8,91 @@ clear
 A = imread('text1.jpg');
 A = im2double(A);
 sizeA = size(A);
-widthSize = Size(A,1);
-heightSize = Size(A,2);
+widthSize = sizeA(2);
+heightSize = sizeA(1);
 
 B = A;
 sizeB = sizeA;
 
 overlap = 8;
 
-M = zeros((widthSize-overlap)+widthSize, heightSize);
+finalImg = zeros(heightSize, (widthSize*2)-overlap,3);
+sizeIF = size(finalImg);
+
+leftPart = A(:, widthSize-overlap+1:widthSize, :);
+rightPart = B(:, 1:overlap, :);
+sizeLeft = size(leftPart);
+
+overlapErr = overlapError(leftPart, rightPart);
+
+M = zeros(sizeLeft(1), sizeLeft(2));
 sizeM = size(M);
+Toto = M;
 
-firstPartM = a(:, heightSize-overlap+1:heightSize,:);
-secondPartM = a(:, 1:overlap, :);
+for x = 1:sizeM(2)
+    M(1,x) = overlapErr(1,x);
+end
 
-overlapErr = overlapError(firstPartM, secondPartM);
+C = M;
 
+for y = 2:sizeM(1)
+    for x = 2:sizeM(2)
+        if(x == 1)
+            M(y,x) = overlapError(y,x) + min([M(y-1,x),M(y-1,x+1)]);
+            argMin = min([M(y-1,x),M(y-1,x+1)]);
+            if(argMin == M(y-1,x))
+                C(y,x) = x;
+            elseif(argMin == M(y-1,x+1))
+                C(y,x) = x+1;
+            end
+        elseif(x == sizeM(2))
+            M(y,x) = overlapError(y,x) + min([M(y-1,x-1),M(y-1,x)]);
+            argMin = min([M(y-1,x-1),M(y-1,x)]);
+            if(argMin == M(y-1,x-1))
+                C(y,x) = x-1;
+            elseif(argMin == M(y-1,x))
+                C(y,x) = x;
+            end
+        else
+            M(y,x) = overlapError(y,x) + min([M(y-1,x-1),M(y-1,x),M(y-1,x+1)]);
+            argMin = min([M(y-1,x-1),M(y-1,x),M(y-1,x+1)]);
+            if(argMin == M(y-1,x-1))
+                C(y,x) = x-1;
+            elseif(argMin == M(y-1,x))
+                C(y,x) = x;
+            elseif(argMin == M(y-1,x+1))
+                C(y,x) = x+1;
+            end
+        end
+    end
+end
 
+minI = M(sizeM(1),2);
+I = 2;
+for x=3:sizeM(2)
+    tmp = M(sizeM(1),x);
+    if(minI > tmp)
+        minI = tmp;
+        I = x;
+        test = I
+    end
+end
+
+for y = heightSize:-1:1
+    Toto(y,I)=1;
+    for x = 1:sizeIF(2)
+        if(x<widthSize-overlap+1+I)
+            finalImg(y,x,:) = A(y,x,:);
+        elseif(x>widthSize-overlap+1+I)
+            xB = x-(widthSize-overlap+1);
+            finalImg(y,x,:) = B(y,xB,:);
+        end
+    end
+    I = C(y,I);
+end
+
+figure(1)
+imagesc(finalImg)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %labelcost: delta c = ( 0 1 )
--- TIA/tp3/GCMex/overlapError.m
+++ TIA/tp3/GCMex/overlapError.m
@@ -1,12 +1,6 @@
 function [ e ] = overlapError(part1, part2)
 %OVERLAPERROR Summary of this function goes here
 %   Detailed explanation goes here
-    e = zeros(size(part1,1), size(part1,2), 1);
-    sizeE = size(e);
-    for x=1:sizeE(1)
-        for y=1:sizeE(2)
-            e(x,y) = sum(part1(x,y,:)-part2(x,y,:).^2,3);
-        end
-    end
+    e = (sum(part1,3)-sum(part2,3)).^2;
 end
 
